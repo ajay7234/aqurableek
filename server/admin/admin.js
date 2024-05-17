0e5ad398f2ac97c8c65b9b8862cd60d554503bd1
bhavinsen <senbhavin@gmail.com>
2024-05-17 10:44:33 +0530

    removed server

--- server/admin/admin.js
+++
@@ -1,8 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("../json/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://bj3d-56ec0-default-rtdb.firebaseio.com",
-});
